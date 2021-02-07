0beb587c4e154d56377e96c3b174fc765d522898
Amin Mmohammadi <31366908+Swiman@users.noreply.github.com>
2021-02-07 19:53:15 +0330

    Add files via upload

---
+++ a2/cs231n/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+extensions = [
+    Extension('im2col_cython', ['im2column_cython.pyx'],
+              include_dirs=[numpy.get_include()]),
+]
+
+setup(ext_modules=cythonize(extensions), )
